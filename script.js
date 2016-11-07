d919c3ccd4c88cdfcb1106e9dea51d19a92aaf47
Pat-Ayres <patayres.pa@gmail.com>
2016-11-07 15:39:25 -0500

    add PouchDb getting started todo app

--- script.js
+++ script.js
@@ -1,39 +1,157 @@
 (function () {
 
-'use strict';
+  'use strict';
 
-var $ = document.querySelector.bind(document);
+  var $ = document.querySelector.bind(document);
+  var ENTER_KEY = 13;
+  var newTodoDom = document.getElementById('new-todo');
+  var syncDom = document.getElementById('sync-wrapper');
 
-// IndexedDB
+  // LevelDB
 
-var db = new PouchDB('mydb-idb');
+  var NodePouchDB = require('pouchdb');
 
-db.info().then(function (info) {
-  $('#idb').innerHTML = '&#10004; We can use PouchDB with IndexedDB!';
-}).catch(function (err) {
-  $('#idb').innerHTML = 'Error for IndexedDB';
-});
+  var leveldbDB = new NodePouchDB('mydb-leveldb');
 
-// WebSQL
+  var remoteCouch = false;
 
-var websqlDB = new PouchDB('mydb-websql', {adapter: 'websql'});
+  leveldbDB.info(function(err,info){
+    leveldbDB.changes({
+      since: info.update_seq,
+      live:true
+    }).on('change',showTodos);
+  });
 
-websqlDB.info().then(function (info) {
-  $('#websql').innerHTML = '&#10004; We can use PouchDB with WebSQL!';
-}).catch(function (err) {
-  $('#websql').innerHTML = 'Error for WebSQL';
-});
+  leveldbDB.info().then(function (info) {
+    $('#leveldb').innerHTML = '&#10004; We can use PouchDB with LevelDB!';
+  }).catch(function (err) {
+    $('#leveldb').innerHTML = 'Error for LevelDB';
+  });
 
-// LevelDB
+  function addToDo(text) {
+    var todo = {
+      _id: new Date().toISOString(),
+      title: text,
+      completed: false
+    };
+    leveldbDB.put(todo).then(function (response) {
+      console.log("Success: ", response);
+    }).catch(function (err) {
+      console.log("Failure: ", err);
+    });
+  }
 
-var NodePouchDB = require('pouchdb');
+  function showTodos() {
+    leveldbDB.allDocs({ include_docs: true, descending: true }).then(function (doc) {
+      redrawTOdosUI(doc.rows);
+    }).catch(function (err) {
+      console.log("failure: ", err);
+    });
+  }
 
-var leveldbDB = new NodePouchDB('mydb-leveldb');
+  function checkboxChanged(todo, event) {
+    todo.completed = event.target.checked;
+    console.log(todo);
+    leveldbDB.put(todo);
+  }
 
-leveldbDB.info().then(function (info) {
-  $('#leveldb').innerHTML = '&#10004; We can use PouchDB with LevelDB!';
-}).catch(function (err) {
-  $('#leveldb').innerHTML = 'Error for LevelDB';
-});
+  function deleteButtonPressed(todo) {
+    leveldbDB.remove(todo);
+  }
 
+  function todoBlurred(todo, event) {
+    var trimmedText = event.target.value.trim();
+    if (!trimmedText){
+      leveldbDB.remove(todo);
+    } else {
+      todo.title = trimmedText;
+      leveldbDB.put(todo);
+    }
+  }
+
+  function sync() {
+
+  }
+
+  function createTodoListItem(todo) {
+    var checkbox = document.createElement('input');
+    checkbox.className = 'toggle';
+    checkbox.type = 'checkbox';
+    checkbox.addEventListener('change', checkboxChanged.bind(this, todo));
+
+    var label = document.createElement('label');
+    label.appendChild(document.createTextNode(todo.title));
+    label.addEventListener('dblclick', todoDblClicked.bind(this, todo));
+
+    var deleteLink = document.createElement('button');
+    deleteLink.className = 'destroy';
+    deleteLink.addEventListener('click', deleteButtonPressed.bind(this, todo));
+
+    var divDisplay = document.createElement('div');
+    divDisplay.className = 'view';
+    divDisplay.appendChild(checkbox);
+    divDisplay.appendChild(label);
+    divDisplay.appendChild(deleteLink);
+
+    var inputEditTodo = document.createElement('input');
+    inputEditTodo.id = 'input_' + todo._id;
+    inputEditTodo.className = 'edit';
+    inputEditTodo.value = todo.title;
+    inputEditTodo.addEventListener('keypress', todoKeyPressed.bind(this, todo));
+    inputEditTodo.addEventListener('blur', todoBlurred.bind(this, todo));
+
+    var li = document.createElement('li');
+    li.id = 'li_' + todo._id;
+    li.appendChild(divDisplay);
+    li.appendChild(inputEditTodo);
+
+    if (todo.completed) {
+      li.classname += 'complete'; checkbox.checked = true;
+    }
+
+    return li;
+  }
+
+  function todoDblClicked(todo) {
+    var div = document.getElementById('li_' + todo._id);
+    var inputEditTodo = document.getElementById('input_' + todo._id);
+    div.className = 'editing';
+    inputEditTodo.focus();
+  }
+
+  function todoKeyPressed(todo, event) {
+    if (event.keyCode === ENTER_KEY) {
+      var inputEditTodo = document.getElementById('input_' + todo._id); inputEditTodo.blur();
+    }
+  }
+
+  function redrawTOdosUI(todos) {
+    var ul = document.getElementById('todo-list');
+    ul.innerHTML = '';
+    todos.forEach(function (todo) {
+      ul.appendChild(createTodoListItem(todo.doc));
+    });
+  }
+
+  function syncError() {
+    syncDom.setAttribute('data-sync-state', 'error');
+  }
+
+  function newTodoKeyPressHandler(event) {
+    if (event.keyCode === ENTER_KEY) {
+      addToDo(newTodoDom.value);
+      newTodoDom.value = '';
+    }
+  }
+
+  function addEventListeners() {
+    newTodoDom.addEventListener('keypress', newTodoKeyPressHandler, false);
+  }
+
+  addEventListeners();
+  showTodos();
+
+  if (remoteCouch) {
+    sync();
+  }
 })();
